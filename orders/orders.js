92dcedadfdc81473cae18437cf602254227cc893
Christopher Trauco <Chris@Trau.co>
2019-06-24 16:04:31 -0400

    Setup 'orders' service | defined 'Order' model

--- orders/orders.js
+++ orders/orders.js
@@ -2,4 +2,9 @@
 // LOAD EXPRESS
 const express = require('express');
 const app = express();
-const app = express();
+
+const PORT = 5555;
+
+app.listen (PORT, () => {
+    console.log(`Orders services is now listening on port ${PORT}`);
+})
